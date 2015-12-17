7f04e0aeaf3ee39e3be2c16342d14d16ca0b2716
jake <jsandler18@gmail.com>
2015-12-16 21:03:53 -0500

    still git damage control

---
+++ linalg/linalg.h
@@ -0,0 +1,29 @@
+/*This header file defines structures and functions for working with matrices and 
+ * vectors*/
+#if !defined(LINALG_H)
+#define LINALG_H
+
+/*represents a vector.  has an array of m real number elements*/
+typedef struct {
+    double * elements;
+    unsigned int m;
+} Vector;
+
+/*Represents a matrix.  has an array of n pointers to vectors as columns*/
+typedef struct {
+    Vector ** columns;
+    unsigned int n;
+} Matrix;
+
+Vector * new_vector(unsigned int m);
+Vector * scalar_mult(double a, Vector * u);
+Vector * vector_add(Vector * u, Vector * v);
+void free_vector(Vector * u);
+Vector * linear_combo(Matrix * a, Vector * x);
+Matrix * new_matrix(unsigned int m, unsigned int n);
+Matrix * matrix_multiply(Matrix * a, Matrix * b);
+void free_matrix(Matrix * a);
+void print_matrix(Matrix * a);
+void print_vector(Vector * a);
+
+#endif
