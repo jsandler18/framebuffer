e3fa7787b6255aec32de2e6ab6a324912b7a6f74
jake <jsandler18@gmail.com>
2015-12-18 09:56:55 -0500

    modified the mouse shape and readded frame timing

--- fb/fb.c
+++ fb/fb.c
@@ -8,10 +8,14 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include "linalg.h"
+#include <sys/time.h>
 #include <sys/resource.h>
 #include "draw.h"
 
 int main () {  
+    struct timeval start, end, delta;
+    int frametime = 120;
+    double avg = 0;
     Matrix * points, * mouse;
     Vector * shift1, * tmp;
     unsigned int color;
@@ -28,28 +32,19 @@ int main () {
     }
     init_screen(240, 320);
     /*make mouse matrix*/
-    mouse = new_matrix(2,7);
+    mouse = new_matrix(2,4);
 
     mouse->columns[0]->elements[0] = 5;
     mouse->columns[0]->elements[1] = 5;
 
     mouse->columns[1]->elements[0] = 5;
-    mouse->columns[1]->elements[1] = 15;
+    mouse->columns[1]->elements[1] = 20;
 
     mouse->columns[2]->elements[0] = 7;
-    mouse->columns[2]->elements[1] = 13;
+    mouse->columns[2]->elements[1] = 15;
 
-    mouse->columns[3]->elements[0] = 9;
-    mouse->columns[3]->elements[1] = 19;
-
-    mouse->columns[4]->elements[0] = 11;
-    mouse->columns[4]->elements[1] = 19;
-
-    mouse->columns[5]->elements[0] = 9;
-    mouse->columns[5]->elements[1] = 13;
-
-    mouse->columns[6]->elements[0] = 14;
-    mouse->columns[6]->elements[1] = 13;
+    mouse->columns[3]->elements[0] = 13;
+    mouse->columns[3]->elements[1] = 15;
 
 
     /*make matrix of points*/
@@ -70,6 +65,7 @@ int main () {
     shift1 = new_vector(2);
 
     while (1) {
+        if (frametime) gettimeofday(&start, NULL);
         read (mouse_fd, mouse_event, 3);
         button = mouse_event[0] & 0x0f;
         color = pixel_color(0x00, 0xff, 0x00);
@@ -98,11 +94,25 @@ int main () {
         fill_poly(points, pixel_color(0xff, 0xff, 0x00));
         fill_poly(mouse, color);
         swap_buffers();
+        if (frametime){
+            gettimeofday(&end, NULL);
+            frametime--;
+            delta = end;
+            delta.tv_sec -= start.tv_sec;
+            delta.tv_usec -= start.tv_usec;
+            if (delta.tv_usec < 0) {
+                delta.tv_usec += 1000000;
+                delta.tv_sec--;
+            }
+            avg+=delta.tv_sec + .000001*delta.tv_usec;
+        }
+
     }
     while (button != 8) {
         read (mouse_fd, mouse_event, 3);
         button = mouse_event[0] & 0x0f;
     }
+    
 
     free_matrix(points);
     free_matrix(mouse);
@@ -110,6 +120,7 @@ int main () {
     
     restore_screen();
 
+    printf("%lf s", avg/120);
     close(mouse_fd);
     return 0;
 }
